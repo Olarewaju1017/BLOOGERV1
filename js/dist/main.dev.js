284a0a096b2e4a077d4cc4c93282ea33bf9e8a1d
Olarewaju Tajudeen <100471829+Olarewaju1017@users.noreply.github.com>
2022-03-11 21:48:16 +0100

    first push to github

---
+++ js/dist/main.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  dots: false,
+  nav: true
+});
+//# sourceMappingURL=main.dev.js.map
---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(".owl-carousel").owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    dots:false,
+    nav:true,
+  
+
+});
